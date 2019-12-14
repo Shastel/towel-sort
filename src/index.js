55342db91c9edd34bf8b59a50c801c5f50d08bfd
shastel <max@shas.tel>
2019-12-14 17:33:50 +0300

    📓 Add towel sort task

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+// You should implement your task here.
+
+module.exports = function towelSort (matrix) {
+  return [];
+}
--- test.js
+++ test.js
@@ -1,5 +1,74 @@
 const assert = require('assert');
 
-it('true should be true', () => {
-  assert.equal(true, true);
+const towelSort = require('./src/index.js');
+
+describe('.towelSort, should perfrom \'towelSort\'', () => {
+
+  it('Should return empty array if no params passed', () => {
+    assert.deepEqual(towelSort(), []);
+  });
+
+  it('Should return empty array if matrix is empty', () => {
+    assert.deepEqual(towelSort([]), []);
+  });
+
+  it('should do towelSort', () => {
+    const matrix = [
+      [1, 2],
+      [3, 4],
+    ];
+
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 3 ]);
+  });
+
+  it('should do towelSort', () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]);
+  });
+
+  it('should do towelSort', () => {
+    const matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+    ];
+
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]);
+  });
+
+  it('should do towelSort correctrly with nonsquare matrix', () => {
+    const matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+    ];
+
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12,]);
+  });
+
+  it('should do towelSort', () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
+
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, ]);
+  });
+
+  it('should do towelSort', () => {
+    const matrix = [
+        [1, 2, 4],
+        [5, 6, 7, 8],
+        [9, 12],
+    ];
+
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 8, 7, 6, 5, 9, 12 ]);
+  });
+
 });

ef9f127481c0ccabc3c9742607d6d314ad1d1df3
Yury <sendtopepper@gmail.com>
2023-02-20 13:01:22 +0300

    feat: solved but errors, i dont know why

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
-// You should implement your task here.
+/* const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+  ]; */
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let newArr = [];
+  
+  for(let i = 0; i < matrix.length; i++){
+    for(let j = 0; j < matrix[i].length; j++){
+        newArr.push(matrix[i][j]);
+    }
+  }
+  const sorting = newArr.sort(function(a,b){return a - b});
+  return sorting;
+};
+
